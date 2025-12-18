32d50c472ef73e9e91ad023a39d3badf250217cb
Md Hasanuzzaman Rony <hasanuxaman.rony@hotmail.com>
2025-12-18 15:09:29 +0600

    init

---
+++ 01_Day_Introduction/main.js
@@ -0,0 +1,18 @@
+import {
+  firstName,
+  lastName,
+  country,
+  city,
+  age,
+  isMarried,
+  gravity,
+  boilingPoint,
+  PI,
+  name,
+  job,
+  live
+} from './variable.js';
+
+console.log(firstName, lastName, country, city, age, isMarried);
+console.log(gravity, boilingPoint, PI);
+console.log(name, job, live);
