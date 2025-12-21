86586a9ee7ad7b9939d70f9c67b9316883df1690
Md Hasanuzzaman Rony <hasanuxaman.rony@hotmail.com>
2025-12-21 14:59:16 +0600

    page Add

---
+++ 03_Day_Booleans_operators_date/Page.js
@@ -0,0 +1,11 @@
+const page = {
+    title: 'My Page',
+    description: 'This is my page',
+    author: 'John Doe',
+    date: '2025-01-01',
+    content: 'This is my page content',
+}
+
+console.log(page)
+
+
