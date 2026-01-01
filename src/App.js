2f75d3f18515865b9ec7705f6355a0cc08fcb96a
Md Hasanuzzaman Rony <hasanuxaman.rony@hotmail.com>
2026-01-01 13:56:12 +0600

    new add components reacthook form

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import Hello, { HelloClass } from './component/FunctionComponent';
-import Counter from './component/StateDynamicdata';
-import EventHandling from './component/EventHandling';
-import FormHandling from './component/FormHandling';
-import SubmitForm from './component/SubmitForm';
+// import Hello, { HelloClass } from './component/FunctionComponent';
+// import Counter from './component/StateDynamicdata';
+// import EventHandling from './component/EventHandling';
+// import FormHandling from './component/FormHandling';
+// import SubmitForm from './component/SubmitForm';
+
+import AdvancedForm from './component/BasicControlledForm';
 
 function App() {
     return (
         <>
-          <HelloClass element={<h1>HelloClass Component Roni here</h1>} />
+          {/* <HelloClass element={<h1>HelloClass Component Roni here</h1>} />
           <Hello element={<h1>Hello Function Component Roni here</h1>} />
           <Counter />
           <EventHandling/>
           <FormHandling/>
-          < SubmitForm/>
+          < SubmitForm/> */}
+          <AdvancedForm/>
         </>
       );
 }
