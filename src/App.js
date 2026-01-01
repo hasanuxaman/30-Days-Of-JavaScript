1e20fbc5ee995e22f96a46be1a3fb3b90bd7e913
Md Hasanuzzaman Rony <hasanuxaman.rony@hotmail.com>
2026-01-01 16:53:31 +0600

    fix api

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import SubmitForm from './component/SubmitForm';
 import AdvancedForm from './component/BasicControlledForm';
 import HookForm from './component/ReactHookForm';
 import FileUploadForm from './component/FileUpload';
+import MultiStepForm from './component/Multi-StepForm';
 
 function App() {
     return (
@@ -22,6 +23,7 @@ function App() {
           <AdvancedForm/>
           <HookForm/>
           <FileUploadForm/>
+          < MultiStepForm/>
         </>
       );
 }
